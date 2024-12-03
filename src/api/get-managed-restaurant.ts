62753f4e0ebe8d366a94cb6579cce6fc642c1f87
Hugo da Silva Bertoncelo <hugobertoncelo@gmail.com>
2024-12-03 17:13:17 -0300

    create pages

--- src/api/get-managed-restaurant.ts
+++ src/api/get-managed-restaurant.ts
@@ -10,7 +10,7 @@ export interface GetManagedRestaurantResponse {
 }
 
 export async function getManagedRestaurant() {
-    const response = await api.get<GetManagedRestaurantResponse>('/menaged-restaurant')
+    const response = await api.get<GetManagedRestaurantResponse>('/managed-restaurant')
 
     return response.data
 }
