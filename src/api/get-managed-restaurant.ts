f58e1bbb39e85c519fd30b8ff17c9a2d63df0df1
Hugo da Silva Bertoncelo <hugobertoncelo@gmail.com>
2024-12-06 11:39:18 -0300

    ajusts

--- src/api/get-managed-restaurant.ts
+++ src/api/get-managed-restaurant.ts
@@ -1,16 +1,18 @@
-import { api } from "@/lib/axios"
+import { api } from '@/lib/axios'
 
 export interface GetManagedRestaurantResponse {
-    name: string;
-    id: string;
-    createdAt: Date | null;
-    updatedAt: Date | null;
-    description: string | null;
-    managerId: string | null;
+  name: string
+  id: string
+  createdAt: Date | null
+  updatedAt: Date | null
+  description: string | null
+  managerId: string | null
 }
 
 export async function getManagedRestaurant() {
-    const response = await api.get<GetManagedRestaurantResponse>('/managed-restaurant')
+  const response = await api.get<GetManagedRestaurantResponse>(
+    '/managed-restaurant',
+  )
 
-    return response.data
+  return response.data
 }
--- src/components/order-status.tsx
+++ src/components/order-status.tsx
@@ -1,41 +1,44 @@
-export type OrderStatus = 
-    | 'pending'
-    | 'canceled'
-    | 'processing'
-    | 'delivering'
-    | 'delivered'
+type OrderStatus =
+  | 'pending'
+  | 'canceled'
+  | 'processing'
+  | 'delivering'
+  | 'delivered'
 
 interface OrderStatusProps {
-    status: OrderStatus
+  status: OrderStatus
 }
 
 const orderStatusMap: Record<OrderStatus, string> = {
-    pending: 'Pendente',
-    canceled: 'Cancelado',
-    delivered: 'Entregue',
-    delivering: 'Em entrega',
-    processing: 'Em preparo',
+  pending: 'Pendente',
+  canceled: 'Cancelado',
+  processing: 'Em preparo',
+  delivering: 'Em entrega',
+  delivered: 'Entregue',
 }
 
 export function OrderStatus({ status }: OrderStatusProps) {
-    return (
-        <div className="flex items-center gap-2">
-            {status === 'pending' && (
-                <span className="h-2 w-2 rounded-full bg-slate-400" />
-            )}
+  return (
+    <div className="flex items-center gap-2">
+      {['pending'].includes(status) && (
+        <span className="h-2 w-2 rounded-full bg-slate-400" />
+      )}
 
-            {status === 'canceled' && (
-                <span className="h-2 w-2 rounded-full bg-rose-500" />
-            )}
+      {['canceled'].includes(status) && (
+        <span className="h-2 w-2 rounded-full bg-rose-500" />
+      )}
 
-            {status === 'delivered' && (
-                <span className="h-2 w-2 rounded-full bg-emerald-500" />
-            )}
+      {['processing', 'delivering'].includes(status) && (
+        <span className="h-2 w-2 rounded-full bg-amber-500" />
+      )}
 
-            {['processing', 'delivering'].includes(status) && (
-                <span className="h-2 w-2 rounded-full bg-amber-500" />
-            )}
-            <span className="font-medium test-muted-foreground">{orderStatusMap[status]}</span>
-        </div>
-    )
+      {['delivered'].includes(status) && (
+        <span className="h-2 w-2 rounded-full bg-emerald-500" />
+      )}
+
+      <span className="font-medium text-muted-foreground">
+        {orderStatusMap[status]}
+      </span>
+    </div>
+  )
 }
