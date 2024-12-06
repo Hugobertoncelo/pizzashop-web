318e26ecf1bedbfd61e54421d8056aeb7008dfcf
Hugo da Silva Bertoncelo <hugobertoncelo@gmail.com>
2024-12-06 13:16:01 -0300

    ajusts

--- src/pages/app/dashboard/month-orders-amout-card.tsx
+++ src/pages/app/dashboard/month-orders-amout-card.tsx
@@ -1,49 +1,54 @@
-import { getMonthOrdersAmount } from "@/api/get-month-orders-amount";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { useQuery } from "@tanstack/react-query";
-import { Utensils } from "lucide-react";
-import { MetricCardSkeleton } from "./metric-card-skeleton";
+import { useQuery } from '@tanstack/react-query'
+import { Loader2, Utensils } from 'lucide-react'
+
+import { getMonthOrdersAmount } from '@/api/get-month-orders-amount'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+import { CardSkeleton } from './card-skeleton'
 
 export function MonthOrdersAmountCard() {
-    const { data: monthOrdersAmount } = useQuery({
-        queryFn: getMonthOrdersAmount,
-        queryKey: ['metrics', 'month-orders-amount'],
+  const { data: monthOrdersAmount, isFetching: isLoadingMonthOrdersAmount } =
+    useQuery({
+      queryKey: ['metrics', 'month-orders-amount'],
+      queryFn: getMonthOrdersAmount,
     })
-    
-    return (
-        <Card>
-            <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-base font-semibold">Pedidos (mês)</CardTitle>
-                <Utensils className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent className="space-y-1">
-                {monthOrdersAmount ? (
-                    <>
-                        <span className="text-2xl font-bold tracking-light">
-                            {monthOrdersAmount.amount.toLocaleString('pt-BR')}
-                        </span>
-                        <p className="text-xs text-muted-foreground">
-                           {monthOrdersAmount.diffFromLastMonth >= 0 ? (
-                            <>
-                                <span className="text-emerald-500 dark:text-emerald-400">
-                                    +{monthOrdersAmount.diffFromLastMonth}%
-                                </span>{' '}
-                                em relação ao mês passado
-                            </>
-                           ) : (
-                            <>
-                                <span className="text-rose-500 dark:text-rose-400">
-                                    {monthOrdersAmount.diffFromLastMonth}%
-                                </span>{' '}
-                                em relação ao mês passado
-                            </>
-                           )}
-                        </p>
-                    </>
-                ) : (
-                    <MetricCardSkeleton />
-                )}
-            </CardContent>
-        </Card>
-    )
+
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-base font-semibold">Pedidos (mês)</CardTitle>
+        {isLoadingMonthOrdersAmount ? (
+          <Loader2 className="h-4 w-4 animate-spin text-muted-foreground" />
+        ) : (
+          <Utensils className="h-4 w-4 text-muted-foreground" />
+        )}
+      </CardHeader>
+      <CardContent className="space-y-1">
+        {monthOrdersAmount ? (
+          <>
+            <span className="text-2xl font-bold">
+              {monthOrdersAmount.amount.toLocaleString('pt-BR')}
+            </span>
+            <p className="text-xs text-muted-foreground">
+              <span
+                className={
+                  monthOrdersAmount.diffFromLastMonth > 0
+                    ? 'text-emerald-500'
+                    : 'text-red-500'
+                }
+              >
+                {monthOrdersAmount.diffFromLastMonth > 0
+                  ? `+${monthOrdersAmount.diffFromLastMonth}`
+                  : monthOrdersAmount.diffFromLastMonth}
+                %
+              </span>{' '}
+              em relação ao mês passado
+            </p>
+          </>
+        ) : (
+          <CardSkeleton />
+        )}
+      </CardContent>
+    </Card>
+  )
 }
